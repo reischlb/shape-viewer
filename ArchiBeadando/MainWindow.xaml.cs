8363e8d8944dd5a3fa03500bb388c437700de7b4
DanielHun911 <DanielHun911@.github.com>
2022-11-22 00:24:19 +0100

    wpf app

---
+++ ArchiBeadando/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ShapeOperations;
+
+namespace ArchiBeadando
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ImageBrush ib = new ImageBrush();
+            ib.ImageSource = new BitmapImage(new Uri(@"magyarorszag.png", UriKind.Relative));
+            myCanvas.Height = ib.ImageSource.Height;
+            myCanvas.Width = ib.ImageSource.Width;
+            
+            myCanvas.Background = ib;
+            myCanvas.InvalidateVisual();
+            int a = 1;
+
+
+        }
+    }
+}
