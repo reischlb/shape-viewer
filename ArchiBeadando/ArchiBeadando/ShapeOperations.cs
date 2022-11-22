002bc5c95b724fd0d562f205b618b725cb5757d9
benjamin.papp <benjamin.papp@suitsolutions.eu>
2022-11-22 11:52:32 +0100

    wpf initial 2

---
+++ ArchiBeadando/ArchiBeadando/ShapeOperations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ArchiBeadando
+{
+    public class ShapeOperations
+    {
+        List<String> collection;
+
+        public ShapeOperations() { }
+
+        public List<String> addPoint(Point point) {
+
+            return collection;
+        }
+
+        public List<String> modifyPoint(Point point,int id)
+        {
+
+            return collection;
+        }
+
+        public List<String> deletePoint(Point point, int id)
+        {
+
+            return collection;
+        }
+    }
+}
